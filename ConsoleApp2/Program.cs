5eecd955469d1747246e1f4db712383bf61bdf77
owner <owner@C301-04>
2023-09-13 10:52:14 +0800

    加入專案檔案。

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,28 @@
+﻿using ConsoleApp2;
+using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        private static void Main ( string[] args )
+        {
+            List<drink> drinks = new List<drink>();
+
+            //drink drink1 = new drink() { Name = "紅茶", size = "大杯"price = 50 };
+            //drinks.Add( drink1 );
+            drinks.Add(new drink() { Name = "紅茶", Size = "大杯", Price = 50 });
+            drinks.Add(new drink() { Name = "紅茶", Size = "小杯", Price = 30 });
+            drinks.Add(new drink() { Name = "綠茶", Size = "大杯", Price = 50 });
+            drinks.Add(new drink() { Name = "綠茶", Size = "小杯", Price = 30 });
+            drinks.Add(new drink() { Name = "咖啡", Size = "大杯", Price = 60 });
+            drinks.Add(new drink() { Name = "咖啡", Size = "小杯", Price = 40 });
+
+            for(int i = 0;  i < drinks.Count; i++) 
+            {
+                Console.WriteLine($"{drinks[i].Name} {drinks[i].Size} {drinks[i].Price}");
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/drink.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class drink
+    {
+        public string Name { get; set; }
+        public string Size { get; set; }
+        public int Price { get; set; }
+    }
+}
